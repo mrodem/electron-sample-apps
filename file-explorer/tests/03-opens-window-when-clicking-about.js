108785f0c75bca5a5e660ce1a0547220e15667e0
Magne Rodem <magne@rodem.no>
2017-11-29 17:21:15 +0100

    Test that About window is opened

---
+++ file-explorer/tests/03-opens-window-when-clicking-about.js
@@ -0,0 +1,18 @@
+const Application = require('spectron').Application;
+const path = require('path');
+
+const projectPath = path.join(__dirname, '..');
+const app = new Application({
+    path: path.join(projectPath, 'node_modules', '.bin', 'electron'),
+    args: [projectPath],
+});
+
+app.start()
+    .then(() => app.client.waitUntilWindowLoaded())
+    .then(() => app.client.windowByIndex(0).click('a=About'))
+    .then(() => app.client.waitUntilWindowLoaded())
+    .then(() => app.client.windowByIndex(1).browserWindow.getTitle())
+    .then(title => {
+        console.log(title);
+        return app.stop();
+    });
