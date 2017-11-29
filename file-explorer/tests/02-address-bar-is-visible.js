217b31f236d2b8d33dc74c58f2d91883c25fd378
Magne Rodem <magne@rodem.no>
2017-11-29 17:20:52 +0100

    Test that address bar is visible

---
+++ file-explorer/tests/02-address-bar-is-visible.js
@@ -0,0 +1,16 @@
+const Application = require('spectron').Application;
+const path = require('path');
+
+const projectPath = path.join(__dirname, '..');
+const app = new Application({
+    path: path.join(projectPath, 'node_modules', '.bin', 'electron'),
+    args: [projectPath],
+});
+
+app.start()
+    .then(() => app.client.waitUntilWindowLoaded())
+    .then(() => app.client.windowByIndex(0).isVisible('#sidebar'))
+    .then(isVisible => {
+        console.log(isVisible);
+        return app.stop();
+    });
